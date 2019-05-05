9c3e8e396da8d29e0a131881778912f5f12b9a8d
Fernando David Maurício <escritorfernando@hotmail.com>
2019-05-05 22:54:41 +0100

    Add project files.

---
+++ Model/Militar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Militar
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public DateTime  DataNascimento { get; set; }
+        public string BI { get; set; }
+        public string Telefone { get; set; }
+        public string Morada { get; set; }
+
+        public Militar()
+        {
+
+        }
+        public Militar(int id, string nome, DateTime dataNascimento, string bi, string telefone, string morada)
+        {
+            this.ID = id;
+            this.Nome = nome;
+            this.DataNascimento = dataNascimento;
+            this.BI = bi;
+            this.Telefone = telefone;
+            this.Morada = morada;
+        }
+    }
+}
---
+++ Model/Utilizador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Utilizador
+    {
+        public int ID { get; set; }
+        public int IdMilitar { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public int IdNivel { get; set; }
+
+        public Militar Militar { get; set; }
+
+        public Utilizador()
+        {
+
+        }
+        public Utilizador(int id, int idMilitar,  string usuario, string senha, int idNivel)
+        {
+            this.ID = id;
+            this.IdMilitar = idMilitar;
+            this.Usuario = usuario;
+            this.Senha = senha;
+            this.IdNivel = idNivel;
+        }
+
+        public Utilizador(int id, int idMilitar, string usuario, string senha, int idNivel, Militar militar)
+        {
+            this.ID = id;
+            this.IdMilitar = idMilitar;
+            this.Usuario = usuario;
+            this.Senha = senha;
+            this.IdNivel = idNivel;
+            this.Militar = militar;
+        }
+    }
+}
